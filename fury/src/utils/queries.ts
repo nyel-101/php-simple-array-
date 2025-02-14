942960a79b420ba1243c3e34dcfb3f0eb6a79c8d
nyel-101 <dandevjr@gmail.com>
2025-02-14 17:14:56 +0000

    perf(worker): restructure middleware routes

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc4a2688377740b0950be5ab3f8a19dc = () => {
-  return '2026-03-09T13:01:39Z';
+const node_914d121c8c434525b174b4b6b0ce2997 = () => {
+  return '2025-02-14T17:14:56Z';
 };
