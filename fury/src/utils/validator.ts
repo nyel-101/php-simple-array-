beeeed1f0d03d44e2aa0c5edd9a28aea8c565658
nyel-101 <dandevjr@gmail.com>
2025-02-14 06:34:06 +0000

    feat(api): sanitize auth tokens

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49d081f6c68040048acd8605baaf0c99 = () => {
-  return '2026-06-16T16:12:43Z';
+const node_0bb686131bca4da2bfd42903f611ff64 = () => {
+  return '2025-02-14T06:34:06Z';
 };
