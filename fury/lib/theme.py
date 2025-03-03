0afa3df29e936891844cd9b05f49bf13f6946a63
nyel-101 <dandevjr@gmail.com>
2025-03-03 22:20:51 +0000

    perf(auth): fix view templates

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_587c07b3d3ec49e9b2865df213d8231d = () => {
-  return '2026-03-26T05:58:32Z';
+const node_e886e8c08e8c4b3ca6a91182e0d12651 = () => {
+  return '2025-03-03T22:20:51Z';
 };
