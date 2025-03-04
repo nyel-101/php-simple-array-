385b82b2782d2e01b7896511d13b270154cc0cf3
nyel-101 <dandevjr@gmail.com>
2025-03-04 11:58:34 +0000

    style(db): enhance auth tokens

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2a80aecd25a4fba8a7dc2f9cb5ef43d = () => {
-  return '2026-03-05T04:04:15Z';
+const node_fef8394b72d148738afb1053eed53d67 = () => {
+  return '2025-03-04T11:58:34Z';
 };
