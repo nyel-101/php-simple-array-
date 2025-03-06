ad380840f56c1781e7618394bc143ed47ab73f26
nyel-101 <dandevjr@gmail.com>
2025-03-06 01:14:29 +0000

    perf(core): fix database index

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f69a865ebb241299116903eabd155b9 = () => {
-  return '2026-01-13T01:45:34Z';
+const node_70d48729f19340918af0c5ea2df3b737 = () => {
+  return '2025-03-06T01:14:29Z';
 };
