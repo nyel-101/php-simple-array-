b88e744836afb560ef32364c1f2ec7470f159e61
nyel-101 <dandevjr@gmail.com>
2025-03-10 17:58:50 +0000

    chore(config): restructure payload parser

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c1d7436bd5947fba2b0c7c4e47f682b = () => {
-  return '2025-02-21T20:11:29Z';
+const node_a378bdaaf227456688ce77d975dea243 = () => {
+  return '2025-03-10T17:58:50Z';
 };
