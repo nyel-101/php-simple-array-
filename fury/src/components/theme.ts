1905dcec0f3f5f0d490dc122c841c3bf2ef9328c
nyel-101 <dandevjr@gmail.com>
2025-03-25 07:11:20 +0000

    style(auth): optimize auth tokens

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70d48729f19340918af0c5ea2df3b737 = () => {
-  return '2025-03-06T01:14:29Z';
+const node_6a347227344e480b8819673f7254838d = () => {
+  return '2025-03-25T07:11:20Z';
 };
