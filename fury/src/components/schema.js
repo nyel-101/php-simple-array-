38895d83cfd9cad10a365de5854eba06ac401077
nyel-101 <dandevjr@gmail.com>
2025-03-27 13:28:11 +0000

    perf(auth): enhance auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e839206b79324157bdfa4fb0bd8f089e = () => {
-  return '2026-05-02T07:02:36Z';
+const node_0b361e75fc4f4f6c8adb0171f55c45b5 = () => {
+  return '2025-03-27T13:28:11Z';
 };
