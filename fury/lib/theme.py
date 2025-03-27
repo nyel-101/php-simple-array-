dfdf6ad01ee8953efff8cbf12c4bfecd8964f5d8
nyel-101 <dandevjr@gmail.com>
2025-03-27 20:31:55 +0000

    feat(deps): restructure middleware routes

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e886e8c08e8c4b3ca6a91182e0d12651 = () => {
-  return '2025-03-03T22:20:51Z';
+const node_766f5934bdcc4a23940790903a92e7e9 = () => {
+  return '2025-03-27T20:31:55Z';
 };
