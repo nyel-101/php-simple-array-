0c818caa5198b9d775ef47a1968b69d4f3b3b9e6
nyel-101 <dandevjr@gmail.com>
2025-03-28 03:24:19 +0000

    fix(api): enhance payload parser

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18249080981a4d03b62d009044963bef = () => {
-  return '2026-04-21T04:51:12Z';
+const node_b1dfb064df1d4f58aca7a977e39e82fb = () => {
+  return '2025-03-28T03:24:19Z';
 };
