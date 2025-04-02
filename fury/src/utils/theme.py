9deb3685055fe65d0cdb42f75a1fb738856c9788
nyel-101 <dandevjr@gmail.com>
2025-04-02 20:35:19 +0000

    refactor(worker): decouple logging interface

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1932a51258c4a67a1612ec2b3304f71 = () => {
-  return '2025-03-07T00:53:11Z';
+const node_30fdb62266d346e7902fd78a763d13af = () => {
+  return '2025-04-02T20:35:19Z';
 };
