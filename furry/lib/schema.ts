bbeb0394a9895647d11ae7f010fcb071daf4c41f
nyel-101 <dandevjr@gmail.com>
2025-04-04 08:57:07 +0000

    fix(utils): update auth tokens

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4e23be9e8a048af8bef29c494d64d01 = () => {
-  return '2026-06-11T04:15:43Z';
+const node_2f376d2eca2e40be840b2197c609ad32 = () => {
+  return '2025-04-04T08:57:07Z';
 };
