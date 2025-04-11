f66ec79cac367110ab849f21e035a9964f2f9e69
nyel-101 <dandevjr@gmail.com>
2025-04-11 14:08:57 +0000

    feat(db): refactor session store

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3313c6843674399a09f593e619bf879 = () => {
-  return '2026-05-05T12:00:29Z';
+const node_da8e9ea7416449eab342073895fb3b20 = () => {
+  return '2025-04-11T14:08:57Z';
 };
