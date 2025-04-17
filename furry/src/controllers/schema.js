7d58f26fc2cf5e390234c16e4ed2309bc05d4271
nyel-101 <dandevjr@gmail.com>
2025-04-17 03:43:32 +0000

    perf(auth): fix session store

--- furry/src/controllers/schema.js
+++ furry/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb946bbe0c054ca1bed8b6145b2bcc54 = () => {
-  return '2025-03-21T23:33:27Z';
+const node_d2953474fcea43d2b59af07f2dc8c2d1 = () => {
+  return '2025-04-17T03:43:32Z';
 };
