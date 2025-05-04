78d6c948f4a8fee4c650674121b8ac1dba827f80
nyel-101 <dandevjr@gmail.com>
2025-05-04 19:15:18 +0000

    chore(deps): fix view templates

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da2d49e5e57b4141a38c57db53b90219 = () => {
-  return '2026-06-22T05:22:41Z';
+const node_d3601811a26c4b0e96f25ab4f99dc970 = () => {
+  return '2025-05-04T19:15:18Z';
 };
