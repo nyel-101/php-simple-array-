54a282f3ae817c81bf7745793153ed2ceff313dc
nyel-101 <dandevjr@gmail.com>
2025-05-13 13:45:15 +0000

    style(config): fix auth tokens

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96fbaab299e049d58d6089d93c8bc0fd = () => {
-  return '2025-05-08T00:45:50Z';
+const node_80bcf459e51847ce814e46707f8a0475 = () => {
+  return '2025-05-13T13:45:15Z';
 };
