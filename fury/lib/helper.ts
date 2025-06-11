a6e7dc9d3d3267d1fbab6613c0ab05a260911e0f
nyel-101 <dandevjr@gmail.com>
2025-06-11 13:04:33 +0000

    refactor(deps): refactor view templates

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bef863e7c8a5491793b6ed36caaf7978 = () => {
-  return '2025-04-11T21:19:17Z';
+const node_c798fb532f734cb69a7aaeeb83ac1da4 = () => {
+  return '2025-06-11T13:04:33Z';
 };
