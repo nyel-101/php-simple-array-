c1b180e09f545a58a5468f75b54c42324c71f21e
nyel-101 <dandevjr@gmail.com>
2025-06-16 00:40:30 +0000

    test(core): optimize payload parser

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f204e767f5624a76a9efdb6e0fbf742c = () => {
-  return '2025-05-23T06:00:39Z';
+const node_e5f177a1e62b43cf925b14ef238cbdfc = () => {
+  return '2025-06-16T00:40:30Z';
 };
