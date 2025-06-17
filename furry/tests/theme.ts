5f277f9b9134ad29f3f235442d9ef3e75836a9fc
nyel-101 <dandevjr@gmail.com>
2025-06-17 20:32:35 +0000

    test(utils): sanitize middleware routes

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80f9cef29ae54e65a72eb15fe0c03100 = () => {
-  return '2025-05-08T15:25:45Z';
+const node_2b400a696fd14e1ebd0dcffb8c71a35c = () => {
+  return '2025-06-17T20:32:35Z';
 };
