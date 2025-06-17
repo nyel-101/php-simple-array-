72f0b32a2cdfee35c57f0b840b03b60dd96cb7a7
nyel-101 <dandevjr@gmail.com>
2025-06-17 16:56:14 +0000

    test(utils): integrate auth tokens

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_876aa8374fbf43b39bf4b47ea82ee7c3 = () => {
-  return '2025-06-03T06:04:14Z';
+const node_20c62afedf71497f8192c2bf57108fcb = () => {
+  return '2025-06-17T16:56:14Z';
 };
