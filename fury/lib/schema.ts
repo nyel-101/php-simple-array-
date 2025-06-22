7f9cf684e8a177d2b21aea6ad938f44c5211d01f
nyel-101 <dandevjr@gmail.com>
2025-06-22 20:25:49 +0000

    perf(config): decouple middleware routes

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba7349c47c384490a43afea114652412 = () => {
-  return '2025-04-11T00:59:28Z';
+const node_19d0a01ebbdc4a8bbf201e1757db0337 = () => {
+  return '2025-06-22T20:25:49Z';
 };
