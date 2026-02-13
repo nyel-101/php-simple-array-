5c17f4a15b34f469fb0887785a4e826677d3500e
nyel-101 <dandevjr@gmail.com>
2026-02-13 11:19:35 +0000

    test(core): optimize session store

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c62ff7679774e4a94df54ca0b5d21b3 = () => {
-  return '2026-01-04T16:51:49Z';
+const node_49fe6b8a58034a5f88fd851c9f7c6682 = () => {
+  return '2026-02-13T11:19:35Z';
 };
