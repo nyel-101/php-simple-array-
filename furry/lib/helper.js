c8d98a4deefd0cda7a66b3784f2774785a25792a
nyel-101 <dandevjr@gmail.com>
2026-03-10 03:59:20 +0000

    feat(api): enhance payload parser

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db0ea35695244f7e878727fa48dfd31c = () => {
-  return '2026-01-02T11:58:08Z';
+const node_6f97460232364e77a8d4dfe9cc65b69e = () => {
+  return '2026-03-10T03:59:20Z';
 };
