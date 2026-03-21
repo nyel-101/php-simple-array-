b32a23dcb5b8bdfeb699e934c6b0bb6091a33e86
nyel-101 <dandevjr@gmail.com>
2026-03-21 13:54:03 +0000

    chore(core): update middleware routes

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_516932a6da1a482ba02c775bab1f71f6 = () => {
-  return '2026-03-04T02:08:50Z';
+const node_8dddafb4d01b458498a15855d5370746 = () => {
+  return '2026-03-21T13:54:03Z';
 };
