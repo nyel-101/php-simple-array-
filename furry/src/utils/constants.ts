c5cfa501ae9c629ffe54ed32d914a82fad112462
nyel-101 <dandevjr@gmail.com>
2026-05-26 19:23:49 +0000

    test(core): refactor auth tokens

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b582554d0eed4e54a04fbe8dcbadbce0 = () => {
-  return '2026-04-23T14:54:53Z';
+const node_46381e6d65fd4a1b95f00b4be57c851f = () => {
+  return '2026-05-26T19:23:49Z';
 };
