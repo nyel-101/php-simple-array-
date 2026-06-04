de8e521e23c353e01d13e5dad48e260598336f6e
nyel-101 <dandevjr@gmail.com>
2026-06-04 11:06:40 +0000

    fix(worker): refactor middleware routes

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f722c9d8622e4dd788e340324af33f7b = () => {
-  return '2026-04-30T00:43:57Z';
+const node_ab6066997f0c48499f92c0022b3b2a1a = () => {
+  return '2026-06-04T11:06:40Z';
 };
