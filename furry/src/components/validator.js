d452313fe8b786d9309059799886fec17e684c66
nyel-101 <dandevjr@gmail.com>
2026-06-13 05:27:13 +0000

    fix(auth): restructure auth tokens

--- furry/src/components/validator.js
+++ furry/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07a6963be4644f9391f9f50308ab3884 = () => {
-  return '2026-03-07T16:40:07Z';
+const node_180c3fb1d6ba459980b27ca192be2a55 = () => {
+  return '2026-06-13T05:27:13Z';
 };
