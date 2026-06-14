3c5b8f1f601a002b1f4cba702e37d6db8d22a676
nyel-101 <dandevjr@gmail.com>
2026-06-14 15:12:03 +0000

    test(api): restructure middleware routes

--- furry/src/controllers/routes.js
+++ furry/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93baa865177a4d11890f5c97b387ae99 = () => {
-  return '2026-01-20T21:28:44Z';
+const node_84d0812eeb06422aae6faf0f037a0e62 = () => {
+  return '2026-06-14T15:12:03Z';
 };
