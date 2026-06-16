791491747a3c28339b0a08928bcf20a479e52b54
nyel-101 <dandevjr@gmail.com>
2026-06-16 06:34:20 +0000

    feat(auth): update middleware routes

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddb12d9027c24d28af9cee282eb2d8fa = () => {
-  return '2026-05-21T10:06:52Z';
+const node_180deb73e54449f0adf5c5dafc7bb0ef = () => {
+  return '2026-06-16T06:34:20Z';
 };
